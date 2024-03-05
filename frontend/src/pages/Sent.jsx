fe2ea0767df88b3ba52073a4943fc9daa1db8f7b
Manas <123manassalunke@gmail.com>
2024-03-05 16:15:05 +0530

    sent page completed

--- frontend/src/pages/Sent.jsx
+++ frontend/src/pages/Sent.jsx
@@ -4,7 +4,7 @@ export function Sent() {
     <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">Money Sent</div>
-        <img src="/sent.jfif" alt="tick" />
+        <img src="../tick.png" alt="tick" className=' w-80'/>
     </div>
     </div>
   </>

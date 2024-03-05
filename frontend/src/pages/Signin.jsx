52f5da2c82e84a2ebc44af92523e97247359315c
Cactusjuice <96051596+salunkemanas@users.noreply.github.com>
2024-03-05 18:01:58 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -24,7 +24,7 @@ export const Signin = () => {
         }} placeholder="123456" label={"Password"} />
         <div className="pt-4">
           <Button onClick={async ()=>{
-            const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
+            const response = await axios.post("https://payment-app-fvkc.onrender.com/api/v1/user/signin", {
               username,
               password
             })
